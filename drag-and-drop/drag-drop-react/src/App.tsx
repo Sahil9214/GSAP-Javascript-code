0a242302366863f429de484fe18b64ba21155991
SahiL@9214 <utkarshsinghal369@gmail.com>
2024-08-19 12:20:07 +0530

    add netlify link

--- drag-and-drop/drag-drop-react/src/App.tsx
+++ drag-and-drop/drag-drop-react/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import DragDropExample from "./DragDrop";
-
+//!NETLIFY LINK - https://66c2ea48c24898a5429474dd--splendorous-bublanina-9ed6db.netlify.app/
 function App() {
   return (
     <div>
